965ea09f349314818cfbbc811f13e27fd4ebf457
somiar <somiar2010@hotmail.com>
2021-08-02 17:15:53 +0800

    add wallet card

--- src/views/Wallet/Steem.vue
+++ src/views/Wallet/Steem.vue
@@ -1,10 +1,16 @@
 <template>
-  <div></div>
+  <div class="row">
+    <div class="col-xl-4 col-md-6 mb-4" v-for="i of 8" :key="i">
+      <SteemCard/>
+    </div>
+  </div>
 </template>
 
 <script>
+import SteemCard from '@/components/Wallet/cards/SteemCard'
 export default {
-  name: 'Steem'
+  name: 'Steem',
+  components: { SteemCard }
 }
 </script>
 
