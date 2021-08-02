7d64342e7d0d9a79310499b556575f1960705347
somiar <somiar2010@hotmail.com>
2021-08-02 17:27:04 +0800

    update wallet card style

--- src/components/Wallet/cards/SteemCard.vue
+++ src/components/Wallet/cards/SteemCard.vue
@@ -3,8 +3,8 @@
     <div class="top flex-between-center">
       <img src="~@/static/images/logo_small.png" alt="" class="logo" />
       <div class="balance-right flex-full">
-        <div class="d-flex justify-content-between align-items-start font-bold mb-1">
-          <div class="d-flex flex-column align-items-start">
+        <div class="d-flex justify-content-between align-items-start font-bold">
+          <div class="d-flex flex-column align-items-start justify-content-between">
             <div class="font16">STEEM POWER</div>
             <div class="font12 text-grey-light">STEEM</div>
           </div>
@@ -28,10 +28,16 @@ export default {
 @import "src/static/css/card/wallet-card";
 .wallet-card {
   border: 2px solid transparent;
-}
-.wallet-card:hover {
-  box-shadow: 0 2px 20px #FFE973;
-  border: 2px solid #FFDB1B;
-  box-sizing: border-box;
+  &:hover {
+    box-shadow: 0 2px 20px #FFE973;
+    border: 2px solid #FFDB1B;
+    box-sizing: border-box;
+  }
+  .top {
+    margin-bottom: 0;
+  }
+  .btn-group {
+    margin-top: 1rem;
+  }
 }
 </style>
