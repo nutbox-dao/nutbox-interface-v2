37a2621fbd509ed8ba9d01c8da5244a561e5fc19
somiar <somiar2010@hotmail.com>
2021-09-22 11:11:55 +0800

    update token page

--- src/views/Community/Index.vue
+++ src/views/Community/Index.vue
@@ -8,6 +8,16 @@
         </b-nav>
       </div>
       <div class="side-page-view-content">
+        <div class="container">
+          <div class="view-top-header m-view-top-header flex-between-center">
+            <div class="nav-box nav-box-bg">
+              <div class="nav">
+                <b-nav-item to="/community">Community List</b-nav-item>
+                <b-nav-item to="#">Community Tokens</b-nav-item>
+              </div>
+            </div>
+          </div>
+        </div>
         <router-view></router-view>
       </div>
     </template>
