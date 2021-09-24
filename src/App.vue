b814689e718aca6b805b1f062e4e3d6d17fb78e2
wangxi <wangxi8206@sina.com>
2021-09-24 09:17:02 +0800

    move initial code to app.vue

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,112 @@
 </template>
 
 <script>
-// import { updateAllTokensFromBackend } from '@/utils/web3/asset'
+import { LOCALE_KEY } from '@/config'
+import { mapState, mapMutations, mapActions } from 'vuex'
+import { initApis } from '@/utils/commen/api'
+import { isMobile } from '@/utils/commen/util'
+import { setupNetwork, chainChanged } from '@/utils/web3/web3'
+import { accountChanged, getAccounts } from '@/utils/web3/account'
+import { subBlockNum } from '@/utils/web3/block'
+import { getAllCommunities, monitorCommunity } from '@/utils/web3/community'
+import { getAllPools, monitorPools, UpdateApysOfPool } from '@/utils/web3/pool'
+import { handleApiErrCode } from '@/utils/helper'
+import {
+  loadAccounts as loadPolkadotAccounts
+} from '@/utils/polkadot/account'
+import { updateAllTokensFromBackend } from '@/utils/web3/asset'
 export default {
-  mounted () {
-    // updateAllTokensFromBackend();
+  computed: {
+    ...mapState('polkadot', [
+      'isConnected',
+      'allAccounts',
+      'account',
+      'crowdstakings',
+      'communitys',
+      'projects',
+      'clCommunitys'
+    ]),
+    ...mapState(['lang', 'prices']),
+    ...mapState('web3', ['allCommunities', 'stakingFactoryId']),
   },
+  methods: {
+    ...mapMutations('polkadot', [
+      'saveCrowdstakings',
+      'saveCommunitys',
+      'saveProjects',
+      'saveAccount'
+    ]),
+    ...mapMutations('polkadot', ['saveClCommunitys']),
+    ...mapActions('steem', ['setVestsToSteem']),
+    ...mapActions('hive', ['setVestsToHive']),
+    setLanguage (lang) {
+      localStorage.setItem(LOCALE_KEY, lang)
+      this.$store.commit('saveLang', lang)
+      this.$i18n.locale = lang
+    },
+        // BSC data
+    async fetchBscData () {
+      try {
+        this.loading = true
+        getAllCommunities()
+        getAllPools().then(res => {
+          monitorPools()
+        }).catch(console.error)
+        monitorCommunity()
+      } catch (e) {
+        handleApiErrCode(e, (tip, param) => {
+          this.$bvToast.toast(tip, param)
+        })
+      } finally {
+        this.loading = false
+      }
+    }
+  },
+  mounted () {
+    const _this = this
+    window.onresize = () => {
+      return (() => {
+        window.screenWidth = document.body.clientWidth
+        _this.screenWidth = window.screenWidth
+      })()
+    }
+    this.setLanguage(localStorage.getItem(LOCALE_KEY) || 'en')
+    updateAllTokensFromBackend();;
+  },
+  async created () {
+    // BSC data
+    this.fetchBscData()
+    // bsc related
+    try {
+      await getAccounts(true)
+    } catch (e) {
+      console.log('Get accounts fail', e)
+    }
+    try {
+      setupNetwork()
+      chainChanged()
+      accountChanged()
+      subBlockNum()
+    } catch (e) {
+      console.log(533, e)
+    }
+    // get steem vests ratio
+    this.setVestsToSteem()
+    this.setVestsToHive()
+
+    UpdateApysOfPool()
+
+    // 如果是手机端，直接清空账号缓存，用插件中的第一个地址
+    if (isMobile()) {
+      console.log('Is mobile device')
+      this.$store.commit('polkadot/saveAccount', null)
+    }
+
+    // init polkadot apis
+    initApis().then(api => {
+      loadPolkadotAccounts()
+    })
+  }
 }
 </script>
 
