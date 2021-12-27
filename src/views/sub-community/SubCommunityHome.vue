73ce0736c8ebe39c8c718ea7425bfb37cf3ef6c2
somiar <somiar2010@hotmail.com>
2021-12-27 12:51:01 +0800

    update icon

--- src/views/sub-community/SubCommunityHome.vue
+++ src/views/sub-community/SubCommunityHome.vue
@@ -284,7 +284,7 @@ export default {
   .v-line {
     display: inline-block;
     margin: 0 1rem;
-    @include line(1px, 1rem, 0, var(--dividers))
+    @include line(1px, 1rem, 0, var(--text-grey-7))
   }
 }
 
