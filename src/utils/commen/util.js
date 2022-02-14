5eda14e7831d51f9402dd220092e4025eedd8443
wangxi <wangxi8206@sina.com>
2022-02-14 15:18:51 +0800

    fix time show

--- src/utils/commen/util.js
+++ src/utils/commen/util.js
@@ -1,6 +1,6 @@
 export const formatDate = (date) => {
   var dateee = new Date(date).toJSON()
-  return new Date(+new Date(dateee) + 8 * 3600 * 1000).toISOString().replace(/T/g, ' ').replace(/\.[\d]{3}Z/, '')
+  return new Date(dateee).toISOString().replace(/T/g, ' ').replace(/\.[\d]{3}Z/, '')
 }
 
 export function isMobile () {
