95bdded43769c071e3d5ec1affc65e6f17b3c215
wangxi <wangxi8206@sina.com>
2022-02-23 19:57:39 +0800

    change update interval

--- src/utils/web3/block.js
+++ src/utils/web3/block.js
@@ -1,14 +1,15 @@
 import { sleep } from "@/utils/helper";
 import { getProvider, getReadonlyProvider } from '@/utils/web3/ethers'
 import store from "@/store";
+import { BLOCK_SECOND } from '@/constant'
 
 export const subBlockNum = async () => {
   const provider = await getReadonlyProvider();
   while (true) {
     try {
-      const blockNumber = await provider.getBlockNumber();
+      const blockNumber = await provider.getProvider();
       store.commit("web3/saveBlockNum", blockNumber);
     } catch (e) {}
-    await sleep(1);
+    await sleep(BLOCK_SECOND);
   }
 };
