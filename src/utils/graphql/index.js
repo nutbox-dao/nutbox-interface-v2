8939dfc42968d56951e6c9900dde8379571d529d
wangxi <wangxi8206@sina.com>
2022-04-22 09:34:28 +0800

    update graph node

--- src/utils/graphql/index.js
+++ src/utils/graphql/index.js
@@ -3,8 +3,8 @@ import {
 } from 'graphql-request';
 
 // export  const client = new GraphQLClient('https://api.thegraph.com/subgraphs/name/terryyyyyy/walnutinchapel');
-// export  const client = new GraphQLClient('https://api.thegraph.com/subgraphs/name/terryyyyyy/walnutinbsc');
-export const client = new GraphQLClient('https://api.thegraph.com/subgraphs/id/QmfZgxmoS2nV5GC94h7BP35yuurrpLWMCdnzGeKVoqRY52')
+export  const client = new GraphQLClient('https://api.thegraph.com/subgraphs/name/terryyyyyy/walnutinbsc');
+// export const client = new GraphQLClient('https://api.thegraph.com/subgraphs/id/QmfZgxmoS2nV5GC94h7BP35yuurrpLWMCdnzGeKVoqRY52')
 // export  const client = new GraphQLClient('https://api.thegraph.com/subgraphs/name/terryyyyyy/walnutingoerli');
 // export  const client = new GraphQLClient('https://api.thegraph.com/subgraphs/name/terryyyyyy/walnutinmbase');
 // export  const client = new GraphQLClient('https://walnut-graph.nutbox.app/subgraphs/name/terryyyyyyy/walnutingoerli');
@@ -13,3 +13,4 @@ export const client = new GraphQLClient('https://api.thegraph.com/subgraphs/id/Q
 // rest-ful from our service
 export const restClient = new GraphQLClient('https://rest-center.nutbox.app/v1/common/search')
 // export const restClient = new GraphQLClient('/rest-graph')
+ 
