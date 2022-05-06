798e3fb7e66781b100405ab9dcb63e8f0f6a189e
wangxi <wangxi8206@sina.com>
2022-05-06 17:59:52 +0800

    update nft icon

--- src/views/sub-community/Index.vue
+++ src/views/sub-community/Index.vue
@@ -18,7 +18,7 @@
                 <span>Farming</span>
               </b-nav-item>
               <b-nav-item to="/sub-community/erc1155">
-                <i class="menu-icon farming-icon" />
+                <i class="menu-icon nft-icon" />
                 <span>NFT Stake</span>
               </b-nav-item>
               <b-nav-item to="/sub-community/nut-power">
@@ -80,7 +80,7 @@
                     <span>Farming</span>
                   </b-nav-item>
                   <b-nav-item to="/sub-community/erc1155">
-                    <i class="menu-icon farming-icon" />
+                    <i class="menu-icon nft-icon" />
                     <span>NFT Stake</span>
                   </b-nav-item>
                   <b-nav-item to="/sub-community/nut-power">
@@ -374,6 +374,9 @@ export default {
 .dashboard-icon {
   background-image: url("~@/static/images/menu-dashboard.svg");
 }
+.nft-icon {
+  background-image: url("~@/static/images/menu-nft.svg");
+}
 .active {
   .home-icon {
     background-image: url("~@/static/images/menu-home-active.svg");
@@ -401,6 +404,9 @@ export default {
   }
   .dashboard-icon {
     background-image: url("~@/static/images/menu-dashboard-active.svg");
+  }
+  .nft-icon {
+    background-image: url("~@/static/images/menu-nft-active.svg")
   }
 }
 </style>
