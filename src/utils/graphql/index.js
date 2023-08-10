de01828141e32377fd25441d310861d4cee6dc8e
wangxi <wangxi8206@sina.com>
2023-08-10 13:29:01 +0800

    fix

--- src/utils/graphql/index.js
+++ src/utils/graphql/index.js
@@ -11,6 +11,6 @@ export  const client = new GraphQLClient('https://api.thegraph.com/subgraphs/nam
 // export  const client = new GraphQLClient('https://walnut-graph.nutbox.app/subgraphs/name/terryyyyyyy/walnutingoerli');
 
 // rest-ful from our service
-// export const restClient = new GraphQLClient('https://linea-graph.nutbox.app/v1/common/search')
-export const restClient = new GraphQLClient('/rest-graph')
+export const restClient = new GraphQLClient('https://linea-graph.nutbox.app/v1/common/search')
+// export const restClient = new GraphQLClient('/rest-graph')
  
