e8a7a6f5f454c827cebdcbd01c4f851257e02886
wangxi <wangxi8206@sina.com>
2024-07-17 11:06:53 +0800

    change bsc graph node

--- src/utils/graphql/index.js
+++ src/utils/graphql/index.js
@@ -3,8 +3,8 @@ import {
 } from 'graphql-request';
 
 // export  const client = new GraphQLClient('https://api.thegraph.com/subgraphs/name/terryyyyyy/walnutinchapel');
-export  const client = new GraphQLClient('https://bsc-graph.nutbox.app/subgraphs/name/terryyyyyyy/walnutinbsc');
-// export const client = new GraphQLClient('https://api.thegraph.com/subgraphs/id/QmfZgxmoS2nV5GC94h7BP35yuurrpLWMCdnzGeKVoqRY52')
+// export  const client = new GraphQLClient('https://bsc-graph.nutbox.app/subgraphs/name/terryyyyyyy/walnutinbsc');
+export const client = new GraphQLClient('https://api.studio.thegraph.com/query/76695/walnutinbsc/version/latest')
 // export  const client = new GraphQLClient('https://api.thegraph.com/subgraphs/name/terryyyyyy/walnutingoerli');
 // export  const client = new GraphQLClient('https://api.thegraph.com/subgraphs/name/terryyyyyy/walnutinmbase');
 // export  const client = new GraphQLClient('https://walnut-graph.nutbox.app/subgraphs/name/terryyyyyyy/walnutingoerli');
