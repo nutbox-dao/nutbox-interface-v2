2f97ee60a44164c95efb995323c3339ad64ebd2a
wangxi <wangxi8206@sina.com>
2024-07-17 11:11:56 +0800

    update arbi graph node

--- src/utils/graphql/index.js
+++ src/utils/graphql/index.js
@@ -2,8 +2,8 @@ import {
   GraphQLClient
 } from 'graphql-request';
 
-// export  const client = new GraphQLClient('https://api.thegraph.com/subgraphs/name/terryyyyyy/walnutinchapel');
-export  const client = new GraphQLClient('https://arb-graph.nutbox.app/subgraphs/name/walnut/walnutinarb');
+export  const client = new GraphQLClient('https://api.studio.thegraph.com/query/76695/wanlutinarbitrum/version/latest');
+// export  const client = new GraphQLClient('https://arb-graph.nutbox.app/subgraphs/name/walnut/walnutinarb');
 // export const client = new GraphQLClient('https://api.thegraph.com/subgraphs/id/QmfZgxmoS2nV5GC94h7BP35yuurrpLWMCdnzGeKVoqRY52')
 // export  const client = new GraphQLClient('https://api.thegraph.com/subgraphs/name/terryyyyyy/walnutingoerli');
 // export  const client = new GraphQLClient('https://api.thegraph.com/subgraphs/name/terryyyyyy/walnutinmbase');
